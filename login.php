2201b7edaa0c9c8fe7b76f38e14714fcdc873978
samuel viana <samuelviana2626@gmail.com>
2020-01-15 16:15:45 -0800

    erro persistente

--- login.php
+++ login.php
@@ -7,16 +7,16 @@
 
     if (isset($_POST['nome']) && !empty($_POST['nome'])) {
         $nome = stripslashes($_POST['nome']);
-        $senha = stripslashes($_POST['senha']);
+        $senha = md5(stripslashes($_POST['senha']));
         
         $query = $conn->prepare("SELECT * FROM usuarios WHERE nome = :nome AND senha = :senha");
         $query->execute([
             'nome' => $nome,
             'senha' => $senha
         ]);
-        
         if ($query->rowCount() > 0) {
-            $dado = $query->fetch();    
+            $dado = $query->fetch(); 
+            var_dump($dado);   
             $_SESSION['id'] = $dado['id'];
             $_SESSION['nome'] = $nome;
             header("Location: painel.php");
@@ -27,7 +27,4 @@
         }
 
     } 
-    
-
-  
 ?>
